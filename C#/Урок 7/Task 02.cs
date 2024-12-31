ae92e2a8066221f9e53b2c99361184933a3c2f35
Demian Blogan <105989117+demianblogan@users.noreply.github.com>
2024-12-31 15:03:55 +0200

    Додав урок 7

---
+++ C#/Урок 7/Task 02.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main()
+    {
+        System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+        System.Console.WriteLine("Я назавжди запам'ятаю ті 15 секунд тишини з тобою");
+    }
+}
