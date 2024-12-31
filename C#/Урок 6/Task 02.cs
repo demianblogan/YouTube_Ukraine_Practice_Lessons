061ab1c2393bf1f85e214a645ff034d2c08fbdf2
Demian Blogan <105989117+demianblogan@users.noreply.github.com>
2024-12-31 14:56:48 +0200

    Додав урок 7

---
+++ C#/Урок 6/Task 02.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main()
+    {
+        System.Console.Write("Demian Blogan");
+
+        // Відмінність у тому, що метод WriteLine після виводу тексту на екран переводить курсор
+        // на новий рядок, а метод Write цього не робить.
+    }
+}
---
+++ C#/Урок 6/Task 04.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("This pain is always with me.");
+        System.Console.WriteLine("Every time I remember her, my heart is being broken.");
+        System.Console.WriteLine("I just can't live without that soul...");
+    }
+}
