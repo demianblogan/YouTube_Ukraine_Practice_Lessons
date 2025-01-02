5731025834f99639e2d091d1b70449f64410ddc9
Demian Blogan <105989117+demianblogan@users.noreply.github.com>
2025-01-02 12:44:26 +0200

    Delete C# directory

--- C#/Урок 6/Task 06.cs
+++
@@ -1,7 +0,0 @@
-﻿class Program
-{
-    static void Main()
-    {
-        System.Console.WriteLine("Я назавжди запам'ятаю ті 15 секунд тишини з тобою");
-    }
-}
--- C#/Урок 8/Task 05.cs
+++
@@ -1,7 +0,0 @@
-﻿class Program
-{
-    static void Main()
-    {
-        System.Console.Write("***********\n***********\n***********\n***********\n");
-    }
-}
