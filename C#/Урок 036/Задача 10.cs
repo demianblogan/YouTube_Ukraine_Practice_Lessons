5bc72a931146d5e62da0e8f93c9c15e51ebe8112
Demian Blogan <105989117+demianblogan@users.noreply.github.com>
2025-01-16 16:37:24 +0200

    Update Задача 10.cs

--- C#/Урок 036/Задача 10.cs
+++ C#/Урок 036/Задача 10.cs
@@ -1,57 +1,54 @@
-﻿class Program
+class Program
 {
     static void Main()
     {
-        while (true)
+        System.Console.Write("Enter first number: ");
+        double number1 = System.Convert.ToDouble(System.Console.ReadLine());
+        System.Console.Write("Enter second number: ");
+        double number2 = System.Convert.ToDouble(System.Console.ReadLine());
+
+        System.Console.WriteLine("1 - addition");
+        System.Console.WriteLine("2 - subtraction");
+        System.Console.WriteLine("3 - multiplication");
+        System.Console.WriteLine("4 - division");
+
+        System.Console.Write("Choose operation: ");
+        char operation = System.Convert.ToChar(System.Console.ReadLine());
+
+        double result;
+
+        switch (operation)
         {
-            System.Console.Write("Enter first number: ");
-            double number1 = System.Convert.ToDouble(System.Console.ReadLine());
-            System.Console.Write("Enter second number: ");
-            double number2 = System.Convert.ToDouble(System.Console.ReadLine());
+            case '1':
+                result = number1 + number2;
+                System.Console.WriteLine($"{number1} + {number2} = {result}");
+                break;
 
-            System.Console.WriteLine("1 - addition");
-            System.Console.WriteLine("2 - subtraction");
-            System.Console.WriteLine("3 - multiplication");
-            System.Console.WriteLine("4 - division");
+            case '2':
+                result = number1 - number2;
+                System.Console.WriteLine($"{number1} - {number2} = {result}");
+                break;
 
-            System.Console.Write("Choose operation: ");
-            char operation = System.Convert.ToChar(System.Console.ReadLine());
+            case '3':
+                result = number1 * number2;
+                System.Console.WriteLine($"{number1} * {number2} = {result}");
+                break;
 
-            double result;
+            case '4':
+                if (number2 == 0)
+                {
+                    System.Console.WriteLine("You can't divide by zero");
+                }
+                else
+                {
+                    result = number1 / number2;
+                    System.Console.WriteLine($"{number1} / {number2} = {result}");
+                }
+                break;
 
-            switch (operation)
-            {
-                case '1':
-                    result = number1 + number2;
-                    System.Console.WriteLine($"{number1} + {number2} = {result}");
-                    break;
-
-                case '2':
-                    result = number1 - number2;
-                    System.Console.WriteLine($"{number1} - {number2} = {result}");
-                    break;
-
-                case '3':
-                    result = number1 * number2;
-                    System.Console.WriteLine($"{number1} * {number2} = {result}");
-                    break;
-
-                case '4':
-                    if (number2 == 0)
-                    {
-                        System.Console.WriteLine("You can't divide by zero");
-                    }
-                    else
-                    {
-                        result = number1 / number2;
-                        System.Console.WriteLine($"{number1} / {number2} = {result}");
-                    }
-                    break;
-
-                default:
-                    System.Console.WriteLine("Incorrect character. Try again");
-                    break;
-            }
+            default:
+                System.Console.WriteLine("Incorrect character.");
+                break;
         }
     }
 }
