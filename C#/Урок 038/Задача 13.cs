a1508dfb8eaf96068f1ca9bdb54daf1a4acf886f
Demian Blogan <105989117+demianblogan@users.noreply.github.com>
2025-01-17 13:13:25 +0200

    Update Задача 13.cs

--- C#/Урок 038/Задача 13.cs
+++ C#/Урок 038/Задача 13.cs
@@ -1,7 +1,9 @@
-﻿class Program
+class Program
 {
     static void Main()
     {
+        const decimal DEPOSIT_FACTOR = 0.05M;
+
         System.Console.Write("Enter initial sum: ");
         decimal initialSum = System.Convert.ToDecimal(System.Console.ReadLine());
 
@@ -9,11 +11,15 @@
         int years = System.Convert.ToInt32(System.Console.ReadLine());
 
         decimal sum = initialSum;
-        for (int year = 1; year <= years; year++)
+
+        int currentYear = 1;
+        while (currentYear <= years)
         {
-            System.Console.Write($"In {year} year(s) sum will be: {sum:C} + ({sum:C} + {sum:C} * 0.05) = ");
-            sum += sum + sum * 0.05M;
+            System.Console.Write($"In {currentYear} year(s) sum will be: {sum:C} + ({sum:C} + {sum:C} * {DEPOSIT_FACTOR}) = ");
+            sum += sum + sum * DEPOSIT_FACTOR;
             System.Console.WriteLine($"{sum:C}");
+
+            currentYear++;
         }
     }
 }
