1da0c0aa5c0883adc4afe1218b091958b0ebdf76
Demian Blogan <105989117+demianblogan@users.noreply.github.com>
2025-01-22 10:42:41 +0200

    Update Задача 02.cs

--- C#/Урок 049 - Багатовимірні масиви/Задача 02.cs
+++ C#/Урок 049 - Багатовимірні масиви/Задача 02.cs
@@ -1,4 +1,4 @@
-﻿class Program
+class Program
 {
     static void Main()
     {
@@ -21,10 +21,8 @@
         // Calculate sum:
         int sum = 0;
         for (int i = 0; i < array.GetLength(0); i++)
-        {
             for (int j = 0; j < array.GetLength(1); j++)
                 sum += array[i, j];
-        }
 
         // Output sum:
         System.Console.WriteLine($"Sum of all array's elements = {sum}");
