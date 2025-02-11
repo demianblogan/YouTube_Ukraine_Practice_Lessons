bc0b96e0c2799f60ab756aafb34f4b6afd0b0571
Demian Blogan <105989117+demianblogan@users.noreply.github.com>
2025-02-11 16:59:55 +0200

    Додав уроки 41-50

---
+++ C++/Урок 041 - Багатовимірні масиви/Задача 01.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	const unsigned int ROWS = 3, COLUMNS = 4;
+	int array[ROWS][COLUMNS] =
+	{
+		{ 10, 20, 30, 40 },
+		{ 50, 60, 70, 80 },
+		{ 11, 12, 13, 14 }
+	};
+
+	for (unsigned int i = 0; i < ROWS; i++)
+	{
+		for (unsigned int j = 0; j < COLUMNS; j++)
+		{
+			std::cout << array[i][j] << "\t";
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
